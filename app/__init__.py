b8d6e9e922f7083357d902b5cfa8264458dd54c1
MatheusFael <matheus.faelson@academico.ifpb.edu.br>
2024-09-15 23:38:24 -0300

    login almost implemented

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 
 # Carregando as configurações do arquivo config.py na raiz do projeto
 app.config.from_object('config')
+
+# configurando LoginManager
+lm = LoginManager(app)
 
 # Inicializando o banco de dados
 db = SQLAlchemy(app)
@@ -15,4 +20,5 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # Importando as rotas
+from app.models import tables
 from app.controllers import default
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,9 +1,48 @@
-from app import app 
-from flask import render_template
-
-@app.route("/index/<user>")
-@app.route("/", defaults = {'user': None})
-def index(user):
-    return render_template('index.html', user = user)
+from app import app , db
+from flask_login import login_user
+from flask import render_template, flash
+from app.models.tables import User
+from app.models.forms import LoginForm
 
 
+
+
+@app.route("/index")
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+
+        user = User.query.filter_by(username = form.username.data).first()
+        if user and user.password == form.data.password:
+            login_user(user)
+           
+        else:
+            return 'USER NOT FOUND'
+    else:
+        print(form.errors)
+    return render_template('login.html', form = form)
+
+@app.route('/teste/<info>')
+@app.route('/teste', defaults = {'info': None})
+def teste(info):
+    r = User.query.filter_by(username='faelson').first()
+    db.session.delete(r)
+    db.session.commit()
+    return 'OK'
+
+
+
+
+
+
+
+# @app.route("/index/<user>")
+# @app.route("/", defaults = {'user': None})
+# def index(user):
+#     return render_template('index.html', user = user)
